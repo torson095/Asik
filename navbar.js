c5b04246232b7367036f57d3901fdab0d1541af7
torson095 <torson095@gmail.com>
2026-02-25 11:52:10 +0700

    update

--- navbar.js
+++ navbar.js
@@ -1,6 +1,6 @@
 // Fungsi untuk memuat navbar
 function loadNavbar() {
-  fetch('/navbar.html')
+  fetch('navbar.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('navbar-container').innerHTML = data;
